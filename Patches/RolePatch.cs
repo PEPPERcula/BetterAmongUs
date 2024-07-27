81a970012fdb9f3c346c5e19ab4f69b04bdad49e
D1GQ <yourboylax@gmail.com>
2024-07-27 18:43:17 -0500

    NoisemakerRolePatch

---
+++ Patches/RolePatch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+
+namespace BetterAmongUs.Patches;
+
+class RolePatch
+{
+    [HarmonyPatch(typeof(NoisemakerRole))]
+    class NoisemakerRolePatch
+    {
+        [HarmonyPatch(nameof(NoisemakerRole.OnDeath))]
+        [HarmonyPrefix]
+        public static bool NotifyOfDeath_Prefix(NoisemakerRole __instance)
+        {
+            if (__instance.Player == null || !__instance.Player.IsAlive())
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
