60d192ae44868fe78f710b2310815b7c7a05449c
D1GQ <yourboylax@gmail.com>
2024-07-31 08:50:08 -0500

    Modded protocol warning

--- Patches/GamePlayManagerPatch.cs
+++ Patches/GamePlayManagerPatch.cs
@@ -5,12 +5,17 @@ namespace BetterAmongUs.Patches;
 
 class GamePlayManager
 {
+    public static string ModdedProtocolWarning = $"<b><color=#ff1100><size=125%><align=\"center\">Warning</size>\n" +
+    "Modded Protocol Is Enabled!</align></color></b>\n<size=120%> </size>\n"
+        + "While <b><color=#4f92ff>Modded Protcol</b></color> is enabled, the built-in server-sided anti cheat is disabled, be sure to have <b><color=#4f92ff>Anti Cheat</b></color> enabled in settings.\n"
+        + "Also while enabled your lobby will not be viewable to the public and only other <b><color=#4f92ff>Modded Protocol</b></color> players can find your lobby, normal players are still able to join off codes!";
+
     [HarmonyPatch(typeof(LobbyBehaviour))]
     public class LobbyBehaviourPatch
     {
         [HarmonyPatch(nameof(LobbyBehaviour.OnDestroy))]
         [HarmonyPrefix]
-        private static void Prefix(/*LobbyBehaviour __instance*/)
+        private static void OnDestroy_Prefix(/*LobbyBehaviour __instance*/)
         {
             if (GameStates.IsInGame)
             {
@@ -19,7 +24,7 @@ class GamePlayManager
         }
         [HarmonyPatch(nameof(LobbyBehaviour.Start))]
         [HarmonyPostfix]
-        private static void Postfix(/*LobbyBehaviour __instance*/)
+        private static void Start_Postfix(/*LobbyBehaviour __instance*/)
         {
             _ = new LateTask(() =>
             {
@@ -27,7 +32,21 @@ class GamePlayManager
                 {
                     RPC.SyncAllNames(force: true);
                 }
+
+                if (Main.ModdedProtocol.Value)
+                {
+                    Utils.AddChatPrivate(ModdedProtocolWarning, overrideName: " ");
+                }
             }, 1.5f, "LobbyBehaviourPatch SyncAllNames");
+        }
+
+        // Disabled annoying music
+        [HarmonyPatch(nameof(LobbyBehaviour.Update))]
+        [HarmonyPostfix]
+        public static void Update_Postfix(/*LobbyBehaviour __instance*/)
+        {
+            if (Main.DisableLobbyTheme.Value)
+                SoundManager.instance.StopSound(LobbyBehaviour.Instance.MapTheme);
         }
     }
 
