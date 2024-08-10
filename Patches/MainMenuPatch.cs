0f988f30574b801f172d119cdcc50a44b8cd8125
D1GQ <yourboylax@gmail.com>
2024-08-10 17:59:22 -0500

    fix error on popup

--- Patches/MainMenuPatch.cs
+++ Patches/MainMenuPatch.cs
@@ -28,8 +28,9 @@ internal class MainMenuPatch
                 var icon = "<color=#278720>♻</color>";
                 var warning = "<color=#e60000>⚠</color>";
                 FileChecker.HasShownPopUp = true;
+                DisconnectPopup.Instance.gameObject.SetActive(true);
                 DisconnectPopup.Instance._textArea.enableWordWrapping = false;
-                DisconnectPopup.Instance.ShowCustom($"{lines}\n<b><size=200%>{icon}<color=#0ed400>Better Among Us</color>{icon}</size></b>\n<color=#757575><u><size=150%>{warning}<color=#8f0000>{FileChecker.UnauthorizedReason}</color>{warning}</size></u>\n\n<color=white>\n{OnlineMsg}\n{lines}");
+                DisconnectPopup.Instance._textArea.text = $"{lines}\n<b><size=200%>{icon}<color=#0ed400>Better Among Us</color>{icon}</size></b>\n<color=#757575><u><size=150%>{warning}<color=#8f0000>{FileChecker.UnauthorizedReason}</color>{warning}</size></u>\n\n<color=white>\n{OnlineMsg}\n{lines}";
             }
         }
     }
