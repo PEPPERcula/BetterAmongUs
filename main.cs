5c7c544ba16a4a855ae467e456c84eb613f14235
D1GQ <yourboylax@gmail.com>
2024-08-10 23:23:59 -0500

    update date

--- main.cs
+++ main.cs
@@ -26,7 +26,7 @@ public class Main : BasePlugin
     public const bool IsHotFix = true;
     public const string PluginGuid = "com.d1gq.betteramongus";
     public const string PluginVersion = "1.0.0";
-    public const string ReleaseDate = "08.10.2024"; // mm/dd/yyyy
+    public const string ReleaseDate = "08.11.2024"; // mm/dd/yyyy
     public const string Github = "https://github.com/D1GQ/BetterAmongUs-Public";
     public const string Discord = "https://discord.gg/vjYrXpzNAn";
 
