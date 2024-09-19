8bfc68a7d496f6506e1d4ab96b80d8876e3e94bf
D1GQ <yourboylax@gmail.com>
2024-09-19 15:26:07 -0500

    BetterAccountInfo

---
+++ Modules/BetterAccountInfo.cs
@@ -0,0 +1,13 @@
+﻿
+namespace BetterAmongUs;
+
+public class BetterAccountInfo
+{
+    public string? PUID;
+    public string? FriendCode;
+    public string? OverHeadTag;
+    public string? OverHeadColor;
+    public bool? IsDev;
+    public bool? IsSponsor;
+    public int? SponsorTier;
+}
