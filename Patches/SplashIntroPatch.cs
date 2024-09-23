3354f2719f9482f36a174cc5f9691d2dc272faf9
D1GQ <yourboylax@gmail.com>
2024-09-23 18:56:18 -0500

    Fix intro issues and re add intro skip

--- Patches/SplashIntroPatch.cs
+++ Patches/SplashIntroPatch.cs
@@ -7,7 +7,6 @@ class SplashIntroPatch
 {
     public static bool BetterIntro = false;
     public static bool IsReallyDoneLoading = false;
-    private static float ShowTime = 0f;
     private static GameObject BetterLogo;
 
     [HarmonyPatch(typeof(SplashManager))]
@@ -24,27 +23,20 @@ class SplashIntroPatch
         [HarmonyPrefix]
         public static bool Update_Prefix(SplashManager __instance)
         {
-            ShowTime += Time.deltaTime;
-
-            if (ShowTime > 6f && BetterIntro)
+            if (Time.time - __instance.startTime > 2f && BetterIntro)
             {
                 UnityEngine.Object.Destroy(__instance.logoAnimFinish.GetComponent<AudioSource>());
             }
 
-            /*
             if (Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.Mouse1))
             {
-                if (ShowTime > 2.5f && !IsReallyDoneLoading)
+                if (Time.time - __instance.startTime > 2f || BetterIntro)
                 {
                     IsReallyDoneLoading = true;
-                    __instance.startedSceneLoad = true;
-                    __instance.sceneChanger.BeginLoadingScene();
                     __instance.sceneChanger.AllowFinishLoadingScene();
-                    __instance.logoAnimFinish.gameObject.SetActive(false);
-                    __instance.loadingObject.SetActive(true);
+                    __instance.startedSceneLoad = true;
                 }
             }
-            */
 
             if (__instance.doneLoadingRefdata && !__instance.startedSceneLoad && Time.time - __instance.startTime > __instance.minimumSecondsBeforeSceneChange)
             {
@@ -55,22 +47,24 @@ class SplashIntroPatch
                     __instance.logoAnimFinish.gameObject.SetActive(true);
                     GameObject InnerLogo = __instance.logoAnimFinish.transform.Find("LogoRoot/ISLogo").gameObject;
                     BetterLogo = UnityEngine.Object.Instantiate(InnerLogo, InnerLogo.transform.parent);
-                    GameObject.Destroy(InnerLogo);
+                    UnityEngine.Object.Destroy(InnerLogo);
                     BetterLogo.name = "BetterLogo";
                     BetterLogo.GetComponent<SpriteRenderer>().sprite = Utils.LoadSprite("BetterAmongUs.Resources.Images.BetterAmongUs-By-The-Enhanced-Network-Logo.png", 150f);
                     __instance.logoAnimFinish.transform.Find("BlackOverlay").transform.SetLocalY(0f);
                     __instance.loadingObject.SetActive(false);
+
+                    __instance.startedSceneLoad = false;
+                    BetterIntro = true;
+                    return false;
                 }
 
-                __instance.startedSceneLoad = BetterIntro;
-                BetterIntro = true;
+                __instance.startedSceneLoad = true;
 
                 if (__instance.startedSceneLoad && BetterIntro)
                 {
+                    IsReallyDoneLoading = true;
                     __instance.sceneChanger.AllowFinishLoadingScene();
                     __instance.startedSceneLoad = true;
-                    __instance.loadingObject.SetActive(true);
-                    IsReallyDoneLoading = true;
                 }
             }
 
