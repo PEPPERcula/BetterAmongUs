1fd628b7412b627c9f6bce961953c1d1f0e5863f
D1GQ <yourboylax@gmail.com>
2025-10-25 12:16:07 -0500

    update PluginGuid

--- ModInfo.cs
+++ ModInfo.cs
@@ -7,7 +7,7 @@ internal static class ModInfo
     internal const string HotfixNum = "1";
     internal const bool IsHotFix = true;
     internal const string PluginName = "BetterAmongUs";
-    internal const string PluginGuid = "com.ten.betteramongus";
+    internal const string PluginGuid = "com.d1gq.betteramongus";
     internal const string PluginVersion = "1.3.0";
     internal const string ReleaseDate = "10.24.2025"; // mm/dd/yyyy
     internal const string Github = "https://github.com/D1GQ/BetterAmongUs-Public";
