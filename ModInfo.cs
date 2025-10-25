fa14f4557990b688cf8b971bd20748304f665a1b
D1GQ <yourboylax@gmail.com>
2025-10-24 20:57:38 -0500

    Update ModInfo

--- ModInfo.cs
+++ ModInfo.cs
@@ -4,12 +4,12 @@ internal static class ModInfo
 {
     internal static readonly ReleaseTypes ReleaseBuildType = ReleaseTypes.Release;
     internal const string BetaNum = "0";
-    internal const string HotfixNum = "0";
-    internal const bool IsHotFix = false;
+    internal const string HotfixNum = "1";
+    internal const bool IsHotFix = true;
     internal const string PluginName = "BetterAmongUs";
     internal const string PluginGuid = "com.ten.betteramongus";
     internal const string PluginVersion = "1.3.0";
-    internal const string ReleaseDate = "10.19.2025"; // mm/dd/yyyy
+    internal const string ReleaseDate = "10.24.2025"; // mm/dd/yyyy
     internal const string Github = "https://github.com/D1GQ/BetterAmongUs-Public";
     internal const string Discord = "https://discord.gg/vjYrXpzNAn";
 }
