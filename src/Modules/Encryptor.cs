666bf76f896b427a98a9c51d353c05ab7d045452
D1GQ <yourboylax@gmail.com>
2026-01-09 00:21:34 -0600

    move to src

---
+++ src/Modules/Encryptor.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace BetterAmongUs.Modules;
+
+class Encryptor
+{
+    private static readonly byte[] Key = Encoding.UTF8.GetBytes("0123456789ABCDEF0123456789ABCDEF");
+    private static readonly byte[] IV = Encoding.UTF8.GetBytes("ABCDEF0123456789");
+
+    internal static string Encrypt(string input)
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = Key;
+            aes.IV = IV;
+
+            using MemoryStream memoryStream = new();
+            using CryptoStream cryptoStream = new(memoryStream, aes.CreateEncryptor(), CryptoStreamMode.Write);
+            using (StreamWriter streamWriter = new(cryptoStream))
+            {
+                streamWriter.Write(input);
+            }
+            return Convert.ToBase64String(memoryStream.ToArray());
+        }
+    }
+
+    internal static string Decrypt(string input)
+    {
+        using Aes aes = Aes.Create();
+        aes.Key = Key;
+        aes.IV = IV;
+
+        using MemoryStream memoryStream = new(Convert.FromBase64String(input));
+        using CryptoStream cryptoStream = new(memoryStream, aes.CreateDecryptor(), CryptoStreamMode.Read);
+        using StreamReader streamReader = new(cryptoStream);
+        return streamReader.ReadToEnd();
+    }
+}
