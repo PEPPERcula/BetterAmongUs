bc96d92fa86282f3ac083c09c9304da34a8158f9
D1GQ <yourboylax@gmail.com>
2026-01-09 12:42:18 -0600

    Add build metadata

--- src/ModInfo.cs
+++ src/ModInfo.cs
@@ -1,4 +1,6 @@
-﻿namespace BetterAmongUs;
+﻿using System.Reflection;
+
+namespace BetterAmongUs;
 
 internal static class ModInfo
 {
@@ -9,7 +11,17 @@ internal static class ModInfo
     internal const string PluginName = "BetterAmongUs";
     internal const string PluginGuid = "com.d1gq.betteramongus";
     internal const string PluginVersion = "1.3.1";
-    internal const string ReleaseDate = "01.09.2026"; // mm/dd/yyyy
     internal const string Github = "https://github.com/D1GQ/BetterAmongUs-Public";
     internal const string Discord = "https://discord.gg/vjYrXpzNAn";
+    public static string CommitHash => GetAssemblyMetadata("CommitHash");
+    public static string BuildDate => GetAssemblyMetadata("BuildDate");
+
+    private static string GetAssemblyMetadata(string key)
+    {
+        var attribute = Assembly.GetExecutingAssembly()
+            .GetCustomAttributes<AssemblyMetadataAttribute>()
+            .FirstOrDefault(a => a.Key == key);
+
+        return attribute?.Value ?? "unknown";
+    }
 }
